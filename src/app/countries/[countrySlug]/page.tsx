cf1f197c0b4cc72a0da671c9c8a726d10404d7b1
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2025-01-23 18:22:06 +0200

    slug params

--- src/app/countries/[countrySlug]/page.tsx
+++ src/app/countries/[countrySlug]/page.tsx
@@ -1,9 +1,13 @@
-export default function CountryDetails() {
+type Params= {
+  countrySlug: string;
+}
+
+export default async function CountryDetails({ params }: { params: Params }) {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-      <div className="text-black">1111111</div>
+      <div className="text-black">{params?.countrySlug}</div>
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import React from "react";
 import Image from "next/image";
 import "./globals.css";
 import travelIcon from "./../../public/images/icons/luggage.png";
+
 
 
 export const metadata: Metadata = {
@@ -9,23 +11,17 @@ export const metadata: Metadata = {
   description: "Take a time to enjoy the moment",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
     <body className="bg-amber-50">
     <div
       className="h-20 bg-green-primary w-full px-[10px] py-[10px] text-white-primary text-lg overflow-hidden relative">
-
       <div
         className="overflow-hidden w-full h-[65px] translate-x-full absolute flex sm:gap-5 gap-2 items-center sm:top-0 sm:right-0 animate-ticker">
         <Image src={travelIcon} alt="Travel Inspirations" className="h-14 w-14"/>
         <p className="font-semibold sm:text-base text-sm text-center">Welcome to Khrystyna's travel inspirations website </p>
       </div>
-
     </div>
     {children}
     </body>
